d5ee7bafa33abc585f038ce3e85442079442eb42
Lou Lavarro <Lou.Lavarro@ladwp.com>
2020-01-28 14:56:17 -0800

    Add EF Core and EF Core Tools from Nuget. Scaffold models from existing database.

---
+++ DND_Demo/Models/Dndclasses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DND_Demo.Models
+{
+    public partial class Dndclasses
+    {
+        public Dndclasses()
+        {
+            Dndmembers = new HashSet<Dndmembers>();
+        }
+
+        public int Id { get; set; }
+        public string DndclassName { get; set; }
+        public string DndclassDescription { get; set; }
+
+        public virtual ICollection<Dndmembers> Dndmembers { get; set; }
+    }
+}
---
+++ DND_Demo/Models/Dndmembers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DND_Demo.Models
+{
+    public partial class Dndmembers
+    {
+        public int Id { get; set; }
+        public string NetworkId { get; set; }
+        public string RealName { get; set; }
+        public string Dndname { get; set; }
+        public string EmailAddress { get; set; }
+        public int? DndclassId { get; set; }
+        public string FavoriteTvseries { get; set; }
+        public bool? IsDeleted { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public DateTime LastUpdateTimestamp { get; set; }
+
+        public virtual Dndclasses Dndclass { get; set; }
+    }
+}
